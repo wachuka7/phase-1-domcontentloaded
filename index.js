6f1957d584ed6caceef76828a3cd337128ba47d6
wachuka7 <rachaelwachuka7@gmail.com>
2024-01-11 01:27:23 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+// const text =document.querySelector('p');
+// text.innerText= 'JavaScript is so cool. It lets me add text to my page programmatically.';
+document.addEventListener("DOMContentLoaded", function(){ 
+    const newText=document.querySelector('#text');
+    newText.textContent="This is really cool!";
+});
